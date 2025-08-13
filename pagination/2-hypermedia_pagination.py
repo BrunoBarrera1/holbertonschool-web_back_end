ae012ac435c426077837f8d8ba70c7e00d8afcc4
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-08-13 10:42:50 -0300

    a

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -47,8 +47,8 @@ class Server:
 
         return {
                 "page_size": len(data),
-                "page": page
-                "data": data
+                "page": page,
+                "data": data,
                 "next_page": page + 1 if page < total_pages else None,
                 "prev_page": page - 2 if page > 1 else None,
                 "total_pages": total_pages,
