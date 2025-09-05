03b2bc06fbf6a32162edc913de7eb486c082ecd8
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-09-05 14:47:48 -0300

    a

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -6,19 +6,19 @@ function countStudents(path) {
   try {
     // Read the file synchronously
     const data = fs.readFileSync(path, 'utf8');
-    
+
     // Split into lines and filter out empty lines
     const lines = data.split('\n').filter((line) => line.trim() !== '');
-    
+
     // Remove the header line
     const students = lines.slice(1);
-    
+
     // Count total students
     console.log(`Number of students: ${students.length}`);
-    
+
     // Group students by field
     const fields = {};
-    
+
     students.forEach((student) => {
       const [firstname, , , field] = student.split(',');
       if (field) {
@@ -28,13 +28,12 @@ function countStudents(path) {
         fields[field].push(firstname);
       }
     });
-    
+
     // Display students by field
     Object.keys(fields).forEach((field) => {
       const studentList = fields[field];
       console.log(`Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}`);
     });
-    
   } catch (error) {
     throw new Error('Cannot load the database');
   }
